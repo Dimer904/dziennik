e6c7379e785092ca04ec416cf884ed259ed154fa
= <=>
2025-11-26 11:31:16 +0100

    praca 26.11.2025 Dawid Osrowski

--- controllers/StudentsController.php
+++ controllers/StudentsController.php
@@ -15,6 +15,8 @@ require_once "entities\Student.php";
                     return $this->edit();
                 case "delete":
                     return $this->delete();
+                case "save":
+                    return $this->save();
             }
         }
 
@@ -33,6 +35,7 @@ require_once "entities\Student.php";
             if (isset($_GET["form_sent"])){
                 $db_connection = mysqli_connect("localhost", "root", "", "database");
 
+
                 $email = $_POST["email"]??"";
 
                 $query = mysqli_query($db_connection, "INSERT INTO `database`.users (`email`, `password`) VALUES ('$email', MD5('123123'))");
@@ -46,26 +49,13 @@ require_once "entities\Student.php";
             }
             include("templates/student/create.php");
         }
-        function edit(){
-//            if (isset($_GET["form_sent"])){
-//                $db_connection = mysqli_connect("localhost", "root", "", "database");
-//
-//                $email = $_POST["email"]??"";
-//
-//                $query = mysqli_query($db_connection, "INSERT INTO `database`.users (`email`, `password`) VALUES ('$email', MD5('123123'))");
-//
-//                if ($query){
-//                    $user_id = mysqli_insert_id($db_connection);
-//                    $name = $_POST["name"]??"";
-//                    $surname = $_POST["surname"]??"";
-//                    $query = mysqli_query($db_connection, "INSERT INTO `database`.students (`name`, `surname`,`user_id`) VALUES ('$name', '$surname', '$user_id')");
-//                }
-//
-//
-//            }
+        function edit(?int $id=null){
+
+
 
 
             $db_connection = mysqli_connect("localhost", "root", "", "database");
+            $student_id = $id??$_GET["student_id"];
             $query = mysqli_query($db_connection,
                 "
 SELECT 
@@ -76,9 +66,42 @@ SELECT
 FROM `database`.students 
     JOIN `database`.users
     ON `database`.users. id = `database`.students. user_id 
-    WHERE `database`.students. id = '{$_GET['student_id']}'");
+    WHERE `database`.students. id = '{$student_id}'
+    
+    ");
             $student = mysqli_fetch_assoc($query);
             include("templates/student/edit.php");
+        }
+
+        function save(){
+
+            print_r($_POST);
+
+
+            $db_connection = mysqli_connect("localhost", "root", "", "database");
+            //pobieranie dantych
+            if (isset($_POST["id"])){
+                $sql = "
+                    UPDATE
+                        `database`.students  
+                    SET 
+                        `name` = '{$_POST["name"]}',
+                        `surname` = '{$_POST["surname"]}'
+                    WHERE 
+                        `database`.`students`.`id` = {$_POST['id']}
+                 
+                 ";
+
+                mysqli_query($db_connection, $sql);
+                $result = mysqli_affected_rows($db_connection);
+                if ($result==1){
+                    return $this->index();
+                }
+            }
+
+
+            return $this->edit($_POST['id']);
+
         }
         function delete(){}
     }
